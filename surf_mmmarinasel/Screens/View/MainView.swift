4463a9a615e0a8f320030b8912f1b6c47522514a
mmmarinasel <mmmarinasel@icloud.com>
2023-02-08 11:29:51 +0300

    add main view

--- surf_mmmarinasel/Screens/View/MainView.swift
+++ surf_mmmarinasel/Screens/View/MainView.swift
@@ -2,4 +2,75 @@ import UIKit
 
 class MainView: UIView {
     
+    private weak var parent: UIView? = nil
+    private var heightConstraint: NSLayoutConstraint? = nil
+    private var height: CGFloat = 200
+    
+    private enum Direction {
+        case up
+        case down
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let swipeGestureRecognizerUp = UISwipeGestureRecognizer(target: self, action: #selector(didSwipeUp(_:)))
+        swipeGestureRecognizerUp.direction = .up
+        self.addGestureRecognizer(swipeGestureRecognizerUp)
+        let swipeGestureRecognizerDown = UISwipeGestureRecognizer(target: self, action: #selector(didSwipeDown(_:)))
+        swipeGestureRecognizerDown.direction = .down
+        self.addGestureRecognizer(swipeGestureRecognizerDown)
+        self.isUserInteractionEnabled = true
+        
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    @objc private func didSwipeUp(_ sender: UISwipeGestureRecognizer) {
+        self.resize(.up)
+    }
+    
+    @objc private func didSwipeDown(_ sender: UISwipeGestureRecognizer) {
+        self.resize(.down)
+    }
+    
+    private func resize(_ direction: Direction) {
+        guard var heightConstraint = self.heightConstraint else { return }
+        self.removeConstraint(heightConstraint)
+        if direction == .up {
+            self.height += 200
+        } else {
+            self.height -= 200
+        }
+        heightConstraint = self.heightAnchor.constraint(equalToConstant: self.height)
+        NSLayoutConstraint.deactivate([heightConstraint])
+        heightConstraint.constant = self.height
+        self.heightConstraint = heightConstraint
+        NSLayoutConstraint.activate([heightConstraint])
+
+        UIView.animate(withDuration: 0.5) {
+            self.parent?.layoutIfNeeded()
+        }
+    }
+    
+    public func build(_ parent: UIView) {
+        self.parent = parent
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            self.bottomAnchor.constraint(equalTo: parent.bottomAnchor),
+            self.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: parent.trailingAnchor),
+            
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        self.heightConstraint = self.heightAnchor.constraint(equalToConstant: self.height)
+        
+        guard let heightConstraint = self.heightConstraint else { return }
+        NSLayoutConstraint.activate([heightConstraint])
+        self.layer.cornerRadius = 32
+        self.backgroundColor = .green
+    }
 }
--- surf_mmmarinasel/Screens/View/MainViewController.swift
+++ surf_mmmarinasel/Screens/View/MainViewController.swift
@@ -2,10 +2,18 @@ import UIKit
 
 class MainViewController: UIViewController {
 //    typealias CustomView = MainView
-
+    var mainView: MainView = MainView()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .purple
+        self.view.addSubview(self.mainView)
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.mainView.build(self.view)
     }
 }
